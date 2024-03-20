17d692536cc959fd4561d1589ec290c1615c50e4
Raphaël <twizox@gmail.com>
2024-03-20 15:40:40 +0100

    Correction exo 7 TD 2

--- S2/TD02-Pointeurs/exo7.c
+++ S2/TD02-Pointeurs/exo7.c
@@ -2,7 +2,7 @@
 
 
 int getExtremums(int* tab, int n, int** petit, int** grand) {
-	if (tab == NULL) return 1;
+	if (tab == NULL || n == 0) return 1;
 	*petit = tab;
 	*grand = tab;
 	for (int i=1; i < n; i++) {
@@ -18,8 +18,6 @@ int main() {
 	int* petit = NULL, *grand = NULL;
 	int err = getExtremums(tab, 5, &petit, &grand);
 	if (err) printf("Erreur\n");
-	else {
-		printf("Min: %d, max: %d\n", *petit, *grand);
-	}
+	else printf("Min: %d, max: %d\n", *petit, *grand);
 	return 0;
 }
